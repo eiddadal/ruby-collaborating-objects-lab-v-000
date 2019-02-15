11c8e147dda6c7aa8cfab816d8de7287dcaffdec
Dalya Eidda <dalia.eidda@gmail.com>
2019-02-15 21:01:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,8 +26,16 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name) 
-     self.all.detect {|artist| artist.name = name} || Artist.new(name).save
-  end 
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
   
   
   def print_songs 
