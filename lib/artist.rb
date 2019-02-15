0839f8e63a714cc1a3fb4ae4ac5bd86aaecd7946
Dalya Eidda <dalia.eidda@gmail.com>
2019-02-15 19:56:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,10 +16,9 @@ class Artist
     @songs 
   end 
   
-  def add_song(song) 
-    @songs << song
-    song.artist = self
-   song 
+  def add_song(song)
+    @songs << song 
+    song 
   end 
   
   def save
